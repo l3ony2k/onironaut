7f4cb195989576038e33f914992559e5b7a630ae
kehuanwu <selinakhwu@gmail.com>
2024-10-15 11:05:20 -0400

    change from cards to table

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     const headers = document.querySelectorAll('.film-header .film-cell');
+//     const rows = document.querySelectorAll('.film-row:not(.film-header)');
 
-// Placeholder for future interactivity
+//     headers.forEach((header, index) => {
+//         header.addEventListener('click', () => {
+//             const sortedRows = Array.from(rows).sort((rowA, rowB) => {
+//                 const cellA = rowA.querySelectorAll('.film-cell')[index].innerText.toLowerCase();
+//                 const cellB = rowB.querySelectorAll('.film-cell')[index].innerText.toLowerCase();
+                
+//                 if (cellA < cellB) return -1;
+//                 if (cellA > cellB) return 1;
+//                 return 0;
+//             });
+
+//             sortedRows.forEach(row => document.querySelector('.film-table').appendChild(row));
+//         });
+//     });
+// });
